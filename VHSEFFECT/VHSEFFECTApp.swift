22dd674e5e02a9c1d2aaa445296a9d4e728e0ed6
shahzaibali02 <alishahzaib02@gmail.com>
2024-11-19 17:39:03 +0500

    Initial Commit

---
+++ VHSEFFECT/VHSEFFECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  VHSEFFECTApp.swift
+//  VHSEFFECT
+//
+//  Created by Shahzaib Ali on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VHSEFFECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
